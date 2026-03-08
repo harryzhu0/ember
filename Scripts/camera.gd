d870b8af117402a2bf56e4552be154f98d0f14fb
Harry Zhu <zhuh30@barker.college>
2026-03-08 19:07:20 +1100

    Just some stuff :D

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,11 +1,34 @@
 extends Camera3D
 
+@export var smooth_speed: float = 10.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var smoothed_transform: Transform3D
+var target: Transform3D
 
+func _ready():
+	smoothed_transform = global_transform
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Always follow the SpringArm AFTER it updates
+	var parent_arm := get_parent() as SpringArm3D
+	if parent_arm == null:
+		return
+
+	target = parent_arm.global_transform
+
+	# Smooth position
+	smoothed_transform.origin = smoothed_transform.origin.lerp(
+		target.origin,
+		delta * smooth_speed
+	)
+
+	# Smooth rotation (with orthonormalization)
+	var new_basis := smoothed_transform.basis.slerp(
+		target.basis,
+		delta * smooth_speed
+	).orthonormalized()
+
+	smoothed_transform.basis = new_basis
+
+	# Apply the smoothed transform
+	global_transform = smoothed_transform
